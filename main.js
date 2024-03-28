3a66a91651ab2023ee52fcf0a1a60e6957e29c4e
Caio Michels de Freitas <caio_m_freitas@estudante.sesisenai.org.br>
2024-03-28 08:11:27 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let jutsus = require("./jutsus-db.json").jutsus;
+console.log(jutsus);
+
+// console.log(jutsus);
+
+const url = document.URL;
+console.log(url)
+console.log('oi')
+
+// window.addEventListener("load", (e) => {
+//     document.querySelector("#name").textContent =
+//         jutsus[Number(url.slice(-1)) - 1].name ||
+//         "No jutsu found";
+// });
